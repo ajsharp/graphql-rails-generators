2f7fe4fce7e0a9041695060c2ecfcacb653ba528
sshaw <skye.shaw@gmail.com>
2020-01-14 01:26:30 -0500

    Remove unnecessary use of ActiveSupport::Concern

--- lib/generators/gql/gql_generator_base.rb
+++ lib/generators/gql/gql_generator_base.rb
@@ -2,40 +2,36 @@ require 'rails/generators/base'
 
 module Gql
   module GqlGeneratorBase
-    extend ActiveSupport::Concern
+    protected
 
-    included do
-      protected
+    # Generate a namedspaced class name with the mutation prefix
+    def prefixed_class_name(prefix)
+      (class_path + ["#{prefix}_#{file_name}"]).map!(&:camelize).join("::")
+    end
 
-      # Generate a namedspaced class name with the mutation prefix
-      def prefixed_class_name(prefix)
-        (class_path + ["#{prefix}_#{file_name}"]).map!(&:camelize).join("::")
-      end
+    def type_map
+      {
+        integer: 'Int',
+        string: 'String',
+        boolean: 'Boolean',
+        decimal: 'Float',
+        datetime: 'GraphQL::Types::ISO8601DateTime',
+        date: 'GraphQL::Types::ISO8601Date',
+        hstore: 'GraphQL::Types::JSON',
+        text: 'String',
+        json: 'GraphQL::Types::JSON'
+      }
+    end
 
-      def type_map
-        {
-          integer: 'Int',
-          string: 'String',
-          boolean: 'Boolean',
-          decimal: 'Float',
-          datetime: 'GraphQL::Types::ISO8601DateTime',
-          date: 'GraphQL::Types::ISO8601Date',
-          hstore: 'GraphQL::Types::JSON',
-          text: 'String',
-          json: 'GraphQL::Types::JSON'
-        }
-      end
-  
-      def map_model_types(model_name)
-        klass = model_name.constantize
-        associations = klass.reflect_on_all_associations(:belongs_to)
-        bt_columns = associations.map(&:foreign_key)
-  
-        klass.columns
-          .reject { |col| bt_columns.include?(col.name) }
-          .reject { |col| type_map[col.type].nil? }
-          .map { |col| {name: col.name, gql_type: type_map[col.type]} }
-      end
+    def map_model_types(model_name)
+      klass = model_name.constantize
+      associations = klass.reflect_on_all_associations(:belongs_to)
+      bt_columns = associations.map(&:foreign_key)
+
+      klass.columns
+        .reject { |col| bt_columns.include?(col.name) }
+        .reject { |col| type_map[col.type].nil? }
+        .map { |col| {name: col.name, gql_type: type_map[col.type]} }
     end
 
     def root_directory(namespace)
